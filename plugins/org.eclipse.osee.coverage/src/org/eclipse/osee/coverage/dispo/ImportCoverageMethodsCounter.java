ecee975257c5cffa64c0c533c42bfe00993530d0
ddunne <donald.g.dunne@boeing.com>
2012-04-24 10:28:46 -0700

    feature: Add ImportCoverageMethodsFromPackage

---
+++ plugins/org.eclipse.osee.coverage/src/org/eclipse/osee/coverage/dispo/ImportCoverageMethodsCounter.java
@@ -0,0 +1,17 @@
+package org.eclipse.osee.coverage.dispo;
+
+import org.eclipse.osee.framework.jdk.core.type.CountingMap;
+
+public class ImportCoverageMethodsCounter {
+   public int numItems = 0;
+   public int numDispo = 0;
+   public int numMatch = 0;
+   public int numNoMatch = 0;
+   public CountingMap<String> fileToErrorCount = new CountingMap<String>();
+
+   @Override
+   public String toString() {
+      return String.format("Num Items %s; Num Dispo %s; Num Match %s; Num NoMatch %s; Num Files Errored %s", numItems,
+         numDispo, numMatch, numNoMatch, fileToErrorCount.getCounts().size());
+   }
+}
