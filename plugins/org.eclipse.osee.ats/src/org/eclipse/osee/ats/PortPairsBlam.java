09daaf448ad5ac653feb8d9d130eef1998d9742d
David W. Miller <david.w.miller6@boeing.com>
2013-02-04 15:08:21 -0700

    tempory commit to allow for port pairs work
    Change-Id: Ib88873564e545197dc5e96f6ee08a9fa824ce7dd

---
+++ plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/PortPairsBlam.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ats;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.eclipse.osee.framework.core.operation.IOperation;
+import org.eclipse.osee.framework.core.operation.OperationLogger;
+import org.eclipse.osee.framework.ui.skynet.blam.AbstractBlam;
+import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+
+/**
+ * @author Ryan D. Brooks
+ * @author David W. Miller
+ */
+public class PortPairsBlam extends AbstractBlam {
+
+   @Override
+   public IOperation createOperation(VariableMap variableMap, OperationLogger logger) throws Exception {
+      String pairs = variableMap.getString("Porting ID Pairs");
+      return new PortPairsOperation(logger, pairs);
+   }
+
+   @Override
+   public Collection<String> getCategories() {
+      return Collections.singletonList("Admin");
+   }
+
+   @Override
+   public String getXWidgetsXml() {
+      return "<xWidgets><XWidget xwidgetType=\"XText\" fill=\"Vertically\" displayName=\"Porting ID Pairs\" /></xWidgets>";
+   }
+}
