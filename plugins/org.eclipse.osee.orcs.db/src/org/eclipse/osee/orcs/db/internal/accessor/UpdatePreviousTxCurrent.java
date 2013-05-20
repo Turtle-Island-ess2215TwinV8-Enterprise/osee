3893d52411876d12b0778f75e781e90003e841ad
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2013-05-20 16:57:08 -0700

    refactor: Remove caching service from UnsubscribeTransaction
    Change-Id: I427058e83b1b212893bc24c5a0955f23a5fcc2ad

--- plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/accessor/UpdatePreviousTxCurrent.java
+++ plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/accessor/UpdatePreviousTxCurrent.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.osee.framework.core.enums.TxChange;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
-import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.database.IOseeDatabaseService;
 import org.eclipse.osee.framework.database.core.IOseeStatement;
 import org.eclipse.osee.framework.database.core.IdJoinQuery;
@@ -31,15 +30,15 @@ public class UpdatePreviousTxCurrent {
       "SELECT txs.transaction_id, txs.gamma_id FROM osee_join_id idj, %s item, osee_txs txs WHERE idj.query_id = ? and idj.id = item.%s AND item.gamma_id = txs.gamma_id AND txs.branch_id = ? AND txs.tx_current <> ?";
 
    private final IOseeDatabaseService dbService;
-   private final Branch branch;
+   private final int branchId;
    private final OseeConnection connection;
    private IdJoinQuery artifactJoin;
    private IdJoinQuery attributeJoin;
    private IdJoinQuery relationJoin;
 
-   public UpdatePreviousTxCurrent(IOseeDatabaseService dbService, Branch branch, OseeConnection connection) {
+   public UpdatePreviousTxCurrent(IOseeDatabaseService dbService, OseeConnection connection, int branchId) {
       this.dbService = dbService;
-      this.branch = branch;
+      this.branchId = branchId;
       this.connection = connection;
    }
 
@@ -84,9 +83,9 @@ public class UpdatePreviousTxCurrent {
       List<Object[]> updateData = new ArrayList<Object[]>();
       IOseeStatement chStmt = dbService.getStatement(connection);
       try {
-         chStmt.runPreparedQuery(10000, query, queryId, branch.getId(), TxChange.NOT_CURRENT.getValue());
+         chStmt.runPreparedQuery(10000, query, queryId, branchId, TxChange.NOT_CURRENT.getValue());
          while (chStmt.next()) {
-            updateData.add(new Object[] {branch.getId(), chStmt.getLong("gamma_id"), chStmt.getInt("transaction_id")});
+            updateData.add(new Object[] {branchId, chStmt.getLong("gamma_id"), chStmt.getInt("transaction_id")});
          }
       } finally {
          chStmt.close();
