78340c678e1b5cff178ed707459cc6f9bdb0ccb4
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2013-06-14 14:10:46 -0700

    feature[ats_LN54H]: Add Orcs Rest Query performance tests
    Change-Id: I8809f1f5ac453ce21b75f813e772132926fe5978

---
+++ plugins/org.eclipse.osee.orcs.performance.tests/src/org/eclipse/osee/orcs/performance/tests/OseeClientQueryTest.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.orcs.performance.tests;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.eclipse.osee.framework.core.enums.CoreArtifactTypes;
+import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
+import org.eclipse.osee.framework.core.enums.CoreBranches;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.orcs.rest.client.OseeClient;
+import org.eclipse.osee.orcs.rest.client.OseeClientConfig;
+import org.eclipse.osee.orcs.rest.client.OseeClientStandaloneSetup;
+import org.eclipse.osee.orcs.rest.model.search.SearchResult;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+@PerfTest(threads = 4, invocations = 10)
+public class OseeClientQueryTest {
+
+   @Rule
+   public ContiPerfRule rule = new ContiPerfRule();;
+
+   private OseeClient createClient;
+
+   @Before
+   public void testSetup() {
+      OseeClientConfig config = new OseeClientConfig("http://localhost:8095");
+      createClient = OseeClientStandaloneSetup.createClient(config);
+   }
+
+   @Test
+   public void searchForFoldersOnCommon() throws OseeCoreException {
+      int expectedResults = 38;
+
+      SearchResult results =
+         createClient.createQueryBuilder(CoreBranches.COMMON).andIsOfType(CoreArtifactTypes.Folder).getSearchResult();
+      Assert.assertEquals(expectedResults, results.getTotal());
+   }
+
+   @Test
+   public void searchForArtifactWithActionInName() throws OseeCoreException {
+      int expectedResults = 1589;
+
+      SearchResult results =
+         createClient.createQueryBuilder(CoreBranches.COMMON).and(CoreAttributeTypes.Name, "action").getSearchResult();
+      Assert.assertEquals(expectedResults, results.getTotal());
+   }
+}
---
+++ plugins/org.eclipse.osee.orcs.performance.tests/src/org/eclipse/osee/orcs/performance/tests/rule/OseePerformanceTest.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jun 14, 2013
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.orcs.performance.tests.rule;
+
+import org.eclipse.osee.orcs.performance.tests.rule.impl.AbstractTestRule;
+import org.junit.runners.model.FrameworkMethod;
+
+public class OseePerformanceTest extends AbstractTestRule {
+
+   @Override
+   public void onTestStarting(FrameworkMethod method) throws Throwable {
+
+   }
+
+}
